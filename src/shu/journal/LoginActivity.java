bcf9c2a774e4d9d76bcfa240057db20d0d09971c
Nick <fallettan@sacredheart.edu>
2012-10-24 18:56:25 -0400

    First Journal Commit. Login, Register, Reset Password, JournalEntry, and Journal activities created. Login interface started.

---
+++ src/shu/journal/LoginActivity.java
@@ -0,0 +1,21 @@
+package shu.journal;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+      //  getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
---
+++ src/shu/journal/RegisterActivity.java
@@ -0,0 +1,20 @@
+package shu.journal;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class RegisterActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.ac);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+      //  getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
