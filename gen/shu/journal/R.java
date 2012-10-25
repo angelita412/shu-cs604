b3bb1cc9048c0338f84d0bc65f9b4e8dc6dd6e84
Luis Gonzalez <luismaglz@live.com>
2012-10-25 11:27:45 -0400

    rest of the app

--- gen/shu/journal/R.java
+++ gen/shu/journal/R.java
@@ -20,11 +20,17 @@ public final class R {
         public static final int btnRegister=0x7f070004;
         public static final int editPassword=0x7f070002;
         public static final int editUsername=0x7f070001;
-        public static final int menu_settings=0x7f070006;
+        public static final int firt_name=0x7f070006;
+        public static final int last_name=0x7f070007;
+        public static final int menu_settings=0x7f07000b;
+        public static final int password=0x7f070009;
+        public static final int security_question_spinner=0x7f07000a;
         public static final int tvTitle=0x7f070000;
+        public static final int username=0x7f070008;
     }
     public static final class layout {
         public static final int login_activity=0x7f030000;
+        public static final int register_user=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
@@ -33,6 +39,7 @@ public final class R {
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
         public static final int menu_settings=0x7f040002;
+        public static final int regirstration_title=0x7f040004;
         public static final int title_activity_main=0x7f040003;
     }
     public static final class style {
