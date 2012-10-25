a947991af5704ee0b18913813690ceeae0e650b8
Luis Gonzalez <luismaglz@live.com>
2012-10-25 13:50:23 -0400

    Add journal entry interface

--- gen/shu/journal/R.java
+++ gen/shu/journal/R.java
@@ -15,22 +15,25 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int btnForgotPassword=0x7f070005;
-        public static final int btnLogin=0x7f070003;
-        public static final int btnRegister=0x7f070004;
-        public static final int editPassword=0x7f070002;
-        public static final int editUsername=0x7f070001;
-        public static final int firt_name=0x7f070006;
-        public static final int last_name=0x7f070007;
-        public static final int menu_settings=0x7f07000b;
-        public static final int password=0x7f070009;
-        public static final int security_question_spinner=0x7f07000a;
-        public static final int tvTitle=0x7f070000;
-        public static final int username=0x7f070008;
+        public static final int btnForgotPassword=0x7f070007;
+        public static final int btnLogin=0x7f070005;
+        public static final int btnRegister=0x7f070006;
+        public static final int editPassword=0x7f070004;
+        public static final int editUsername=0x7f070003;
+        public static final int firt_name=0x7f070008;
+        public static final int journalPage=0x7f070001;
+        public static final int last_name=0x7f070009;
+        public static final int menu_settings=0x7f07000d;
+        public static final int pageDate=0x7f070000;
+        public static final int password=0x7f07000b;
+        public static final int security_question_spinner=0x7f07000c;
+        public static final int tvTitle=0x7f070002;
+        public static final int username=0x7f07000a;
     }
     public static final class layout {
-        public static final int login_activity=0x7f030000;
-        public static final int register_user=0x7f030001;
+        public static final int add_page=0x7f030000;
+        public static final int login_activity=0x7f030001;
+        public static final int register_user=0x7f030002;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
